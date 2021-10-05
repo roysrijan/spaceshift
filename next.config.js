2597b0d4df6da58ad564a4dcb11200d21f440829
tanumayMatrix <tanumay@matrixnmedia.com>
2021-10-05 16:35:51 +0530

    removed the image from next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  Images: {
-    domains: ['tanumay.d1l94nkhrwiwt.amplifyapp.com'],
-  },
+  // Images: {
+  //   domains: ['tanumay.d1l94nkhrwiwt.amplifyapp.com'],
+  // },
 }
 
